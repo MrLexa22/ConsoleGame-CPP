96a78e84034e0069b6ebc5c181c774c3edbf3ec2
F13b <p.a.kononenkov@gmail.com>
2021-03-14 15:01:31 +0300

    тмыло

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ sf::Music music;
 sf::Music music1;
 
 using namespace std;
-
+//опенкин работать
 const unsigned int DIM1 = 10; //константы, определяющие размеры массивов
 const unsigned int DIM2 = 7;
 string mas[DIM1][DIM2] = { //массив со всем возможными отрядами ///характеристики: атака, защита, здоровье, регенерация, цена, тип атаки
