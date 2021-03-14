87f2227ca18cd89de55fd1626cec1a311de0f212
F13b <p.a.kononenkov@gmail.com>
2021-03-14 19:37:39 +0300

    Kill me pls

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ sf::Music music1;
 
 using namespace std;
 //КОНОПЕНКОВ ПИСАТЬ РАСЫ! Hello
+//I HATE CLION AND VS
 const unsigned int DIM1 = 10; //константы, определяющие размеры массивов
 const unsigned int DIM2 = 7;
 string mas[DIM1][DIM2] = { //массив со всем возможными отрядами ///характеристики: атака, защита, здоровье, регенерация, цена, тип атаки
